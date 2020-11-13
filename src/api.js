207325ebde31efdd05d77ad9872c736c2a159931
Cesare Bek <cesarebek1@gmail.com>
2020-11-13 12:10:05 +0100

    Set API

---
+++ src/api.js
@@ -0,0 +1,37 @@
+const base_url = 'https://api.rawg.io/api/';
+
+//Current Month
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+};
+
+//Current Day
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+//Full date
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+//Popular Games
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+//Final fetching link
+const popularGamesURL = () => `${base_url}${popular_games}`;
+
+console.log(popularGamesURL());
